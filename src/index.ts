b144eaa87941e604256fe8c4bbe6aa54101938fa
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2022-09-11 13:29:48 +0900

    refactor: 型追加

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
 import fetch from 'node-fetch'
 import env from '../.env.json'
 
+type MoralisNft = {
+  token_address: string
+  token_id: string
+  owner_of: string
+  block_number: string
+  block_number_minted: string
+  token_hash: string
+  amount: string
+  contract_type: string
+  name: string
+  symbol: string
+  token_uri: string
+  metadata: string
+  last_token_uri_sync: string
+  last_metadata_sync: string
+}
+
 const main = async () => {
-  const promises: Promise<any>[] = []
+  const promises: Promise<{ result: MoralisNft[] }>[] = []
   for (const address of env.walletAddresses) {
     promises.push(
       fetch(
