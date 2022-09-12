75bed941b449809868590b26cd5c2683ddb279f4
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2022-09-12 17:20:22 +0900

    標準出力を見やすく

--- src/download.js
+++ src/download.js
@@ -16,10 +16,14 @@ const now = () => new Date().toLocaleString(env.locales, { timeZone: env.timeZon
 const download = async (tokenId) => {
   const res = await fetch(tokenId)
   if (!res.ok) {
-    console.log(now(), 'retrying...')
+    console.log(`[${res.status}]${res.statusText}`)
+    process.stdout.write(`     `)
     await new Promise((resolve) => setTimeout(resolve, 10000))
+    process.stdout.write(now())
+    process.stdout.write(` retrying...`)
     return download(tokenId)
   }
+  console.log('done!')
   return res.buffer()
 }
 
@@ -31,7 +35,9 @@ const main = async () => {
       continue
     }
 
-    console.log(now(), tokenId, 'fetching...')
+    process.stdout.write(`${tokenId.toString().padStart(4)} `)
+    process.stdout.write(now())
+    process.stdout.write(` fetching...`)
     const buffer = await download(getIpfsUrl(env.ipfsDirectoryCid, tokenId.toString()))
     fs.writeFileSync(fileName, buffer)
   }
